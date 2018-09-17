14eeb786fd32513a49e06714651cca64aad72880
Anton <a.nikolsqy@gmail.com>
2018-09-18 01:54:44 +0300

    Выполнил задание #16 Личный проект: подробности

--- js/pictures.js
+++ js/pictures.js
@@ -113,6 +113,45 @@
     return fragmentPictures;
   };
 
+  var showOverlay = function (element, addListener) {
+    element.classList.remove('hidden');
+    document.addEventListener('keydown', addListener);
+  };
+
+  var hideOverlay = function (element, removeListener, clearElementValue) {
+    element.classList.add('hidden');
+    document.removeEventListener('keydown', removeListener);
+    if (clearElementValue) {
+      clearElementValue.value = '';
+    }
+  };
+
+  var pictureHideHandler = function () {
+    hideOverlay(bigPicture, pictureHideEscHandler);
+  };
+
+  var pictureHideEscHandler = function (evt) {
+    if (evt.keyCode === ESC_KEY) {
+      hideOverlay(bigPicture, pictureHideEscHandler);
+    }
+  };
+
+  var removeCommentsOverlay = function () {
+    while (containerComments.firstChild) {
+      containerComments.removeChild(containerComments.firstChild);
+    }
+  };
+
+  var renderPicture = function (picture, index) {
+    picture.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      removeCommentsOverlay();
+      showOverlay(bigPicture, pictureHideEscHandler);
+      fillOverlay(listPhotos[index]);
+      containerComments.appendChild(createFragmentComment(listPhotos[index]));
+    });
+  };
+
   var listPhotos = getPhotos();
 
   var templatePicture = document
@@ -120,7 +159,10 @@
     .content.querySelector('.picture');
 
   var bigPicture = document.querySelector('.big-picture');
-  bigPicture.classList.remove('hidden');
+  var closeBigPicture = bigPicture.querySelector('.big-picture__cancel');
+
+  closeBigPicture.addEventListener('click', pictureHideHandler);
+
   bigPicture
     .querySelector('.social__comment-count')
     .classList.add('visually-hidden');
@@ -129,7 +171,58 @@
   var containerPictures = document.querySelector('.pictures');
   var containerComments = document.querySelector('.social__comments');
 
-  fillOverlay(listPhotos[0]);
-  containerComments.appendChild(createFragmentComment(listPhotos[0]));
   containerPictures.appendChild(createFragmentPicture());
+
+  var listPictures = containerPictures.querySelectorAll('.picture');
+
+  for (var i = 0; i < listPictures.length; i++) {
+    var picture = listPictures[i];
+    renderPicture(picture, i);
+  }
+
+  // module4
+
+  var ESC_KEY = 27;
+
+  var fileUploadHandler = function () {
+    showOverlay(filterOverlay, filterHideEscHandler);
+  };
+
+  var filteHideHandler = function () {
+    hideOverlay(filterOverlay, filterHideEscHandler, uploadFile);
+  };
+
+  var filterHideEscHandler = function (evt) {
+    if (evt.keyCode === ESC_KEY) {
+      hideOverlay(filterOverlay, filterHideEscHandler, uploadFile);
+    }
+  };
+
+  var filterCheckHandler = function () {
+    imagePreview.className = '';
+    var currentSelectedFilter = filterList[this.id];
+    imagePreview.classList.add(currentSelectedFilter);
+  };
+
+  var uploadFile = containerPictures.querySelector('#upload-file');
+  var filterOverlay = containerPictures.querySelector('.img-upload__overlay');
+  var closeFilterOverlay = filterOverlay.querySelector('#upload-cancel');
+  var imagePreview = filterOverlay.querySelector('.img-upload__preview img');
+  var filterMode = filterOverlay.querySelectorAll('.effects__radio');
+  var filterSlider = filterOverlay.querySelector('.effect-level__pin');
+  var filterList = {
+    'effect-none': 'effects__preview--none',
+    'effect-chrome': 'effects__preview--chrome',
+    'effect-sepia': 'effects__preview--sepia',
+    'effect-marvin': 'effects__preview--marvin',
+    'effect-phobos': 'effects__preview--phobos',
+    'effect-heat': 'effects__preview--heat'
+  };
+
+  for (var i = 0; i < filterMode.length; i++) {
+    filterMode[i].addEventListener('click', filterCheckHandler);
+  }
+
+  uploadFile.addEventListener('change', fileUploadHandler);
+  closeFilterOverlay.addEventListener('click', filteHideHandler);
 })();
